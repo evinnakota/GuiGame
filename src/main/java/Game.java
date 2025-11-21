c18be27b2e7465259892fcf97b824c74bb479810
eshuv <eshwar.vinnakota@menloschool.org>
2025-11-21 09:40:44 -0800

    Added to the Game class and created hands for players. Simulated getting the first card in the players hands and adding points to their scores based on the cards' values.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -7,11 +7,30 @@ public class Game {
 //        String[] suit = {"C"};
 //        int[] values = {1,2,3};
         Deck d = new Deck(rank,suit,values);
-        Player p1 = new Player("Eshu");
-        System.out.println(d.isEmpty());
         d.shuffle();
-        while (!d.isEmpty()) {
-            System.out.println(d.deal());
+        Player p1 = new Player("Eshu");
+        Player p2 = new Player("Ryan");
+        for (int i = 0; i < 3; i++) {
+            p1.addCard(d.deal());
+            p2.addCard(d.deal());
+        }
+        System.out.println(p1);
+        System.out.println(p2);
+        for (int i = 0; i < 52; i++) {
+            Card c1 = p1.getHand().getFirst();
+            Card c2 = p2.getHand().getFirst();
+            p1.addScore(c1.getValue());
+            p2.addScore(c2.getValue());
+            p1.getHand().removeFirst();
+            p2.getHand().removeFirst();
+            System.out.println(p1);
+            System.out.println(p2);
+            if (p1.getHand().isEmpty() || p1.getHand().isEmpty()) {
+                for (int j = 0; j < 3; j++) {
+                    p1.addCard(d.deal());
+                    p2.addCard(d.deal());
+                }
+            }
         }
 
 
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,10 +1,19 @@
+import java.util.ArrayList;
+
 public class Player {
     private String name;
+    private ArrayList<Card> hand = new ArrayList<>();
     private int score;
+    public Player(String name, ArrayList<Card> hand) {
+        this.name = name;
+        this.score = 0;
+        this.hand = hand;
+    }
     public Player(String name) {
         this.name = name;
         this.score = 0;
     }
+
 
     public String getName() {
         return name;
@@ -22,8 +31,20 @@ public class Player {
         this.score += points;
     }
 
+    public void addCard(Card card) {
+        hand.add(card);
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public void setHand(ArrayList<Card> hand) {
+        this.hand = hand;
+    }
+
     @Override
     public String toString() {
-        return this.name + " has " + this.score + " points!";
+        return this.name + " has " + this.score + " points!" + "\n" + this.name + "'s cards: " + this.hand;
     }
 }
